e94abb5e3f8aa5aae5a26d3064687128a951128f
HasonSheeta <zamora.dan08@gmail.com>
2024-07-22 11:04:58 -0500

    project outline

--- app/src/main/java/edu/utsa/cs3443/connect4/Player.java
+++ app/src/main/java/edu/utsa/cs3443/connect4/Player.java
@@ -1,4 +1,17 @@
 package edu.utsa.cs3443.connect4;
 
 public class Player {
+    private Piece mark;
+
+    public Player(Piece mark) {
+        this.mark = mark;
+    }
+
+    public Piece getMark() {
+        return mark;
+    }
+
+    public void setMark(Piece mark) {
+        this.mark = mark;
+    }
 }
