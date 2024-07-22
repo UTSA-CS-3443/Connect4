b551dcaeec3bd7a94891b597cd64d9737d66f3b7
HasonSheeta <zamora.dan08@gmail.com>
2024-07-22 11:01:23 -0500

    project outline

--- app/src/main/java/edu/utsa/cs3443/connect4/Piece.java
+++ app/src/main/java/edu/utsa/cs3443/connect4/Piece.java
@@ -1,4 +1,17 @@
 package edu.utsa.cs3443.connect4;
 
 public class Piece {
+    private int color;
+
+    public Piece(int color) {
+        this.color = color;
+    }
+
+    public int getColor() {
+        return color;
+    }
+
+    public void setColor(int color) {
+        this.color = color;
+    }
 }
