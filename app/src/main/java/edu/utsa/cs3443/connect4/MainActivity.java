7db0c00556a08a3826cf89a108854abae071021a
Chris <65110077+Rokow6@users.noreply.github.com>
2024-07-23 23:55:32 -0500

    MainActivity, activity_main, HowToPlay, and activity_how_to_play
    MainActivity: added access to all screens
    activity_main: some small adjustments with button ids
    HowToPlayActivity: deleted some unused code
    activity_how_to_play: added the TextView for the information need on the How-To-Play screen as well as the background (blue_gradient).

--- app/src/main/java/edu/utsa/cs3443/connect4/MainActivity.java
+++ app/src/main/java/edu/utsa/cs3443/connect4/MainActivity.java
@@ -1,24 +1,38 @@
 package edu.utsa.cs3443.connect4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
+
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+
+        Button startGame = findViewById(R.id.button);
+        startGame.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, PlayActivity.class);
+            startActivity(intent);
+        });
+        Button how2Play = findViewById(R.id.button1);
+        how2Play.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, HowToPlayActivity.class);
+            startActivity(intent);
+        });
+        Button settings = findViewById(R.id.button2);
+        settings.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, SettingsActivity.class);
+            startActivity(intent);
+        });
+        Button credits = findViewById(R.id.button3);
+        credits.setOnClickListener(view -> {
+            Intent intent = new Intent(MainActivity.this, CreditsActivity.class);
+            startActivity(intent);
         });
     }
 }
