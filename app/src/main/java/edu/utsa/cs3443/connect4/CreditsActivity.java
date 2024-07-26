18de857fbd05791b9433db3b1547dae05e1d6037
HasonSheeta <zamora.dan08@gmail.com>
2024-07-26 12:39:52 -0500

    credits screen

--- app/src/main/java/edu/utsa/cs3443/connect4/CreditsActivity.java
+++ app/src/main/java/edu/utsa/cs3443/connect4/CreditsActivity.java
@@ -1,6 +1,8 @@
 package edu.utsa.cs3443.connect4;
 
+import android.graphics.Paint;
 import android.os.Bundle;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,12 +15,18 @@ public class CreditsActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
         setContentView(R.layout.activity_credits);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
-        });
+
+        TextView projectManager = (TextView) findViewById(R.id.project_manager);
+        projectManager.setPaintFlags(projectManager.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        TextView developer = (TextView) findViewById(R.id.developer);
+        developer.setPaintFlags(developer.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        TextView tester = (TextView) findViewById(R.id.tester);
+        tester.setPaintFlags(tester.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
+
+        TextView designer = (TextView) findViewById(R.id.designer);
+        designer.setPaintFlags(designer.getPaintFlags() | Paint.UNDERLINE_TEXT_FLAG);
     }
 }
