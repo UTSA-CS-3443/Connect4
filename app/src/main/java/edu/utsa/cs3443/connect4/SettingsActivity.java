622aec191b266bf8481280efa728b1008b71c4d1
Blazentesrill <decmpetershack@gmail.com>
2024-07-26 21:58:50 -0500

    here is what i did

--- app/src/main/java/edu/utsa/cs3443/connect4/SettingsActivity.java
+++ app/src/main/java/edu/utsa/cs3443/connect4/SettingsActivity.java
@@ -1,6 +1,8 @@
 package edu.utsa.cs3443.connect4;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,16 +11,23 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class SettingsActivity extends AppCompatActivity {
-
+    private Button menuButton;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_settings);
+        menuButton = findViewById(R.id.menuButton);
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        menuButton.setOnClickListener(view -> {
+            Intent intent = new Intent(SettingsActivity.this, MainActivity.class);
+            startActivity(intent);
+
         });
     }
 }
